caf836b4fac8dcffc7202ffcee057219170cb56c
codershona <falgunimst95@gmail.com>
2020-03-29 19:30:14 +0200

    Switch-Case in the Reducer done

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,35 +4,74 @@ const initialState = {
 
 
 const reducer = (state = initialState, action) => {
-	if (action.type === 'INCREMENT') {
-		return {
+    switch (action.type) {
+    	case 'INCREMENT': 
+    	return {
 			counter: state.counter + 1
 		}
-	}
 
-	if (action.type === 'DECREMENT') {
+		case 'DECREMENT':
+
 		return {
 			counter: state.counter - 1
 		}
-	}
 
-
-	if (action.type === 'ADD') {
+		case 'ADD':
 		return {
-			// counter: state.counter + 10
-			// counter: state.counter + 11
+			
 			counter: state.counter + action.val
 
 		}
-	}
 
-	if (action.type === 'SUBTRACT') {
+		case 'SUBTRACT':
 		return {
-			// counter: state.counter - 8
-			 // counter: state.counter - 9
+
 			 counter: state.counter - action.val
 		}
-	}
+
+
+
+
+    }
+
+
+    	return state;
+
+};
+
+export default reducer;
+
+
+
+	// if (action.type === 'INCREMENT') {
+	// 	return {
+	// 		counter: state.counter + 1
+	// 	}
+	// }
+
+	// if (action.type === 'DECREMENT') {
+	// 	return {
+	// 		counter: state.counter - 1
+	// 	}
+	// }
+
+
+	// if (action.type === 'ADD') {
+	// 	return {
+	// 		// counter: state.counter + 10
+	// 		// counter: state.counter + 11
+	// 		counter: state.counter + action.val
+
+	// 	}
+	// }
+
+	// if (action.type === 'SUBTRACT') {
+	// 	return {
+	// 		// counter: state.counter - 8
+	// 		 // counter: state.counter - 9
+	// 		 counter: state.counter - action.val
+	// 	}
+	// }
 
     // DECREMENT
     // ADD
@@ -41,8 +80,8 @@ const reducer = (state = initialState, action) => {
 
 
 
-	return state;
+// 	return state;
 
-};
+// };
 
-export default reducer;
+// export default reducer;
