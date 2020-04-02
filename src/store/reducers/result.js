f61bc9b5d22dde0c91c7daa9eea483a035a2a02c
codershona <falgunimst95@gmail.com>
2020-04-02 17:05:23 +0200

    Executing Asynchronous Code- Introduction done

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -3,69 +3,38 @@ import * as actionTypes from '../actions';
 
 
 const initialState = {
-	// counter: 0,
+	
 	results: []
 };
 
 
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
-  //   	case actionTypes.INCREMENT: 
-  //         const newState = Object.assign({}, state);
-  //         newState.counter = state.counter + 1;
-
-  //         return newState;
-
-  // //   	return {
-		// // 	counter: state.counter + 1
-		// // }
-
-		// case actionTypes.DECREMENT:
-
-		// return {
-		// 	...state,
-
-		// 	counter: state.counter - 1
-		// }
-
-		// case actionTypes.ADD:
-		// return {
-		// 	...state,
-
-		// 	counter: state.counter + action.val
-
-		// }
-
-		// case actionTypes.SUBTRACT:
-		// return {
-		// 	...state,
-
-		// 	 counter: state.counter - action.val
-		// }
-
+  
 		case actionTypes.STORE_RESULT:
+		
+		// setTimeoout(() => {
+		// 	return {
+
+		// 	}
+
+		// }, 2000);
 		return {
 			...state,
 
  results: state.results.concat({id: new Date(), value: action.result})
 
-			// results: state.results.push()
+			
 		}
 
 		case actionTypes.DELETE_RESULT:
 
-		// const id = 2;
-		// const newArray = [...state.results];
-
-		// // state.results.splice(id, 1)
-		// newArray.results.splice(id, 1)
-
-		// const updatedArray = state.results.filter((result, index) => index !== id);
+		
 		const updatedArray = state.results.filter(result => result.id !== action.resultElId);
 
 		  return {
 		  	...state,
-             // results: newArray
+            
              results: updatedArray
 		  }
 
@@ -83,45 +52,6 @@ export default reducer;
 
 
 
-	// if (action.type === 'INCREMENT') {
-	// 	return {
-	// 		counter: state.counter + 1
-	// 	}
-	// }
-
-	// if (action.type === 'DECREMENT') {
-	// 	return {
-	// 		counter: state.counter - 1
-	// 	}
-	// }
+	
 
 
-	// if (action.type === 'ADD') {
-	// 	return {
-	// 		// counter: state.counter + 10
-	// 		// counter: state.counter + 11
-	// 		counter: state.counter + action.val
-
-	// 	}
-	// }
-
-	// if (action.type === 'SUBTRACT') {
-	// 	return {
-	// 		// counter: state.counter - 8
-	// 		 // counter: state.counter - 9
-	// 		 counter: state.counter - action.val
-	// 	}
-	// }
-
-    // DECREMENT
-    // ADD
-    // SUBTRACT
-
-
-
-
-// 	return state;
-
-// };
-
-// export default reducer;
